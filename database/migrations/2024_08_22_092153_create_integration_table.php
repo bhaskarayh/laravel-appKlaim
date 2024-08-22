65932264c5fe9bee00836d4ad0ea1f8d66c5d09c
bhaskarayh <bhaskarayh@gmail.com>
2024-08-22 16:24:45 +0700

    first commit

---
+++ database/migrations/2024_08_22_092153_create_integration_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('integration', function (Blueprint $table) {
+            $table->id();
+            $table->string('sub_cob');
+            $table->string('penyebab_klaim');
+            $table->date('periode');
+            $table->string('id_wilker');
+            $table->date('tgl_keputusan_klaim');
+            $table->decimal('jumlah_terjamin', 15, 2);
+            $table->decimal('nilai_beban_klaim', 15, 2);
+            $table->string('debet_kredit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('integration');
+    }
+};
